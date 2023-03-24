3b2156332b5406475f9b462570031fe5a42f1cff
Archie James <arjames1618@gmail.com>
2023-03-24 13:02:12 +0000

    hello?

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ import {Box} from "/primitives/box.js"
 import {Viewport} from "/modules/viewport.js"
 
 let viewport = new Viewport("viewport", 'urdf/URDF_models/kuka_iiwa.urdf'); // red 0xff0808
-// let collisions = new Window("collisions");
-// let biem = new Window("biem");
+let collisions = new Window("collisions");
+let biem = new Window("biem");
 // let collisions = new Box(0x08ff08, "collisions"); // green
-let collisions = new Viewport("collisions", 'urdf/URDF_models/SC_3DoF.urdf'); // green
+// let collisions = new Viewport("collisions", 'urdf/URDF_models/SC_3DoF.urdf'); // green
 // let biem = new Box(0x0808ff, "biem"); // blue
-let biem = new Viewport("biem", 'urdf/T12/urdf/arm.urdf'); // blue
+// let biem = new Viewport("biem", 'urdf/T12/urdf/arm.urdf'); // blue
 
 function animate() {
 	// Requesting the browser to call the animate function again on the next frame
